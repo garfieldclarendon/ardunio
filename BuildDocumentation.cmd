9d2fb25dd9379ff2e1c783015652ce42c3308d95
jreilly434 <jr@boystownradio.com>
2019-01-28 12:50:38 -0600

    More documentation updates

--- BuildDocumentation.cmd
+++ BuildDocumentation.cmd
@@ -1,3 +1,4 @@
+copy /Y "Documents\LCSOverview.pdf" "docs\html\garfieldclarendon.github.io\html\Documents"
 D:\"Program Files"\doxygen\bin\doxygen Doxyfile
 call D:"\Program Files\nodejs\nodevars.bat"
 call apidoc -o docs/html/garfieldclarendon.github.io/apidoc/ -i LCSServer/ -f ".*.h$"
